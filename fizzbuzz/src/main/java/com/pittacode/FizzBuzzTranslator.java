c0e717c7f18c135e81f851f3c0f4ba40b06fcb63
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-12-28 19:20:10 +0000

    FizzBuzz - pass it

--- fizzbuzz/src/main/java/com/pittacode/FizzBuzzTranslator.java
+++ fizzbuzz/src/main/java/com/pittacode/FizzBuzzTranslator.java
@@ -3,6 +3,11 @@ package com.pittacode;
 public class FizzBuzzTranslator {
 
     public String translate(int input) {
+
+        if (input % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(input);
     }
 }
