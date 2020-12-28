a001d77c348b85d22c4dd3aea7f0438a54c3d5eb
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-12-28 19:57:36 +0000

    FizzBuzz - Add test

--- fizzbuzz/src/test/java/com/pittacode/AppicationTest.java
+++ fizzbuzz/src/test/java/com/pittacode/AppicationTest.java
@@ -1,8 +1,14 @@
 package com.pittacode;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -11,13 +17,29 @@ import static org.mockito.Mockito.verify;
 class AppicationTest {
 
     @Test
-    void callFizzBuzzTranslator100times() {
+    void whenRunning_callFizzBuzzTranslator100times() {
         var fizzBuzzTranslator = mock(FizzBuzzTranslator.class);
         var underTest = new Application(fizzBuzzTranslator);
 
         underTest.run();
 
-        verify(fizzBuzzTranslator, times(100))
-                .translate(anyInt());
+        verify(fizzBuzzTranslator, times(100)).translate(anyInt());
+    }
+
+
+    @Test
+    void whenRunning_callTranslatorWithSeriesOfInts() {
+        var fizzBuzzTranslator = mock(FizzBuzzTranslator.class);
+        var captor = ArgumentCaptor.forClass(Integer.class);
+        var underTest = new Application(fizzBuzzTranslator);
+
+        underTest.run();
+
+        verify(fizzBuzzTranslator, times(100)).translate(captor.capture());
+        var arguments = captor.getAllValues();
+        var expected = IntStream.range(1, 101)
+                                .boxed()
+                                .toArray(Integer[]::new);
+        assertThat(arguments).containsExactly(expected);
     }
 }
