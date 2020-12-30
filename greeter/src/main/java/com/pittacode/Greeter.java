f979cb5551a8671dea4a694e6f6f8075d2ac34a3
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-12-30 10:36:51 +0000

    Greeter - pass it

--- greeter/src/main/java/com/pittacode/Greeter.java
+++ greeter/src/main/java/com/pittacode/Greeter.java
@@ -5,6 +5,9 @@ import static java.lang.String.format;
 public class Greeter {
 
     public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello, my friend.";
+        }
         return format("Hello, %s.", name);
     }
 }
--- greeter/src/test/java/com/pittacode/GreeterTest.java
+++ greeter/src/test/java/com/pittacode/GreeterTest.java
@@ -1,17 +1,34 @@
 package com.pittacode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GreeterTest {
 
+    private Greeter underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Greeter();
+    }
+
     @Test
     void whenPassesName_greetsUsingThatName() {
-        var underTest = new Greeter();
-
         var result = underTest.greet("Antoni");
 
         assertThat(result).isEqualTo("Hello, Antoni.");
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void whenNoNameIsPassed_greetUsingDefault(String noName) {
+        var result = underTest.greet(noName);
+
+        assertThat(result).isEqualTo("Hello, my friend.");
     }
 }
