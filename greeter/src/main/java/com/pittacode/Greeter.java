03528f4690ea28f8d51c3d1008298f9c836f866e
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-12-31 12:13:26 +0000

    Greeter - pass test and refactor

--- greeter/src/main/java/com/pittacode/Greeter.java
+++ greeter/src/main/java/com/pittacode/Greeter.java
@@ -1,41 +1,65 @@
 package com.pittacode;
 
+import java.util.Arrays;
+
 import static java.lang.String.format;
+import static java.util.function.Predicate.not;
 
 public class Greeter {
 
-    public String greet() {
-        return "Hello, my friend.";
+    public String buildGreeting(String[] names) {
+        var shoutedNames = Arrays.stream(names)
+                                 .filter(this::isUpperCase)
+                                 .toArray(String[]::new);
+        var normalNames = Arrays.stream(names)
+                                .filter(not(this::isUpperCase))
+                                .toArray(String[]::new);
+
+        var normalGreeting = buildNormalGreeting(normalNames);
+        var shoutedGreeting = buildShoutedGreeting(shoutedNames);
+
+        return joinGreetings(normalGreeting, shoutedGreeting);
     }
 
-    public String greet(String name) {
-        if (isEmpty(name)) {
-            return greet();
+    private String joinGreetings(String normalGreeting, String shoutedGreeting) {
+        var greetingBuilder = new StringBuilder(normalGreeting);
+        if (areWeGreetingBothNormallAndShouting(normalGreeting, shoutedGreeting)) {
+            greetingBuilder.append(" AND ");
+        }
+        greetingBuilder.append(shoutedGreeting);
+
+        return greetingBuilder.toString();
+    }
+
+    private boolean areWeGreetingBothNormallAndShouting(String normalGreeting, String shoutedGreeting) {
+        return !normalGreeting.isEmpty() && !shoutedGreeting.isEmpty();
+    }
+
+    private String buildNormalGreeting(String[] names) {
+        if (names.length == 0) {
+            return "";
         }
 
-        var greetingBuilder = new StringBuilder("Hello, ");
+        if (names.length == 1) {
+            return buildNormalGreeting(names[0]);
+        }
 
-
-        return isUpperCase(name)
-                ? greetingBuilder.append(name).append("!").toString().toUpperCase()
-                : greetingBuilder.append(name).append(".").toString();
-    }
-
-    private boolean isEmpty(String name) {
-        return name == null || name.isEmpty();
-    }
-
-    private boolean isUpperCase(String name) {
-        return name.codePoints()
-                   .mapToObj(i -> (char) i)
-                   .allMatch(ch -> !Character.isLetter(ch) || Character.isUpperCase(ch));
-    }
-
-    public String greet(String[] names) {
         if (names.length == 2) {
-            return format("Hello, %s and %s.", names[0], names[1]);
+            return buildNormalGreeting(names[0], names[1]);
         }
 
+        return buildNormalGreetingFor3OrMore(names);
+    }
+
+    private String buildNormalGreeting(String name) {
+        return "Hello, " + name + ".";
+    }
+
+    private String buildNormalGreeting(String name1, String name2) {
+        return format("Hello, %s and %s.", name1, name2);
+    }
+
+    private String buildNormalGreetingFor3OrMore(String[] names) {
         var greetingBuilder = new StringBuilder("Hello, ");
         for (int i = 0; i < names.length - 1; i++) {
             greetingBuilder.append(names[i]);
@@ -46,5 +70,67 @@ public class Greeter {
         greetingBuilder.append(".");
 
         return greetingBuilder.toString();
+    }
+
+    public String buildGreeting(String name) {
+        if (isEmpty(name)) {
+            return buildGenericGreeting();
+        }
+
+
+        return isUpperCase(name)
+                ? buildShoutedGreeting(name)
+                : buildNormalGreeting(name);
+    }
+
+    private String buildGenericGreeting() {
+        return "Hello, my friend.";
+    }
+
+    private String buildShoutedGreeting(String[] names) {
+        if (names.length == 0) {
+            return "";
+        }
+
+        if (names.length == 1) {
+            return buildShoutedGreeting(names[0]);
+        }
+
+        if (names.length == 2) {
+            return buildShoutedGreeting(names[0], names[1]);
+        }
+
+        return buildShoutedGreetingFor3OrMore(names);
+    }
+
+    private String buildShoutedGreeting(String name) {
+        return "HELLO " + name + "!";
+    }
+
+    private String buildShoutedGreeting(String name1, String name2) {
+        return format("HELLO %s AND %s.", name1, name2);
+    }
+
+    private String buildShoutedGreetingFor3OrMore(String[] names) {
+        var greetingBuilder = new StringBuilder("HELLO ");
+        for (int i = 0; i < names.length - 1; i++) {
+            greetingBuilder.append(names[i]);
+            greetingBuilder.append(" AND ");
+        }
+        greetingBuilder.append(" AND ");
+        greetingBuilder.append(names[names.length - 1]);
+        greetingBuilder.append(".");
+
+        return greetingBuilder.toString();
+    }
+
+    private boolean isEmpty(String name) {
+        return name == null || name.isEmpty();
+    }
+
+    private boolean isUpperCase(String name) {
+        return name.codePoints()
+                   .mapToObj(i -> (char) i)
+                   .allMatch(ch -> !Character.isLetter(ch) || Character.isUpperCase(ch));
     }
 }
--- greeter/src/test/java/com/pittacode/GreeterTest.java
+++ greeter/src/test/java/com/pittacode/GreeterTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,7 +18,7 @@ public class GreeterTest {
 
     @Test
     void whenPassesName_greetsUsingThatName() {
-        var result = underTest.greet("Antoni");
+        var result = underTest.buildGreeting("Antoni");
 
         assertThat(result).isEqualTo("Hello, Antoni.");
     }
@@ -29,44 +26,44 @@ public class GreeterTest {
     @ParameterizedTest
     @NullAndEmptySource
     void whenNoNameIsPassed_greetUsingDefault(String noName) {
-        var result = underTest.greet(noName);
+        var result = underTest.buildGreeting(noName);
 
         assertThat(result).isEqualTo("Hello, my friend.");
     }
 
     @Test
     void whenNameInCaps_greetShouting() {
-        var result = underTest.greet("ANTONI");
+        var result = underTest.buildGreeting("ANTONI");
 
-        assertThat(result).isEqualTo("HELLO, ANTONI!");
+        assertThat(result).isEqualTo("HELLO ANTONI!");
     }
 
 
     @Test
     void whenNameWithNonLettersInCaps_greetShouting() {
-        var result = underTest.greet("ANNA-MARIA");
+        var result = underTest.buildGreeting("ANNA-MARIA");
 
-        assertThat(result).isEqualTo("HELLO, ANNA-MARIA!");
+        assertThat(result).isEqualTo("HELLO ANNA-MARIA!");
     }
 
     @Test
     void whenTwoNamesArePassed_greetBothNames() {
-        var result = underTest.greet(new String[]{"Antoni", "Anna-Maria"});
+        var result = underTest.buildGreeting(new String[]{"Antoni", "Anna-Maria"});
 
         assertThat(result).isEqualTo("Hello, Antoni and Anna-Maria.");
     }
 
     @Test
     void whenMultipleNamesArePassed_greetWithAllNamesSeperatedByComma() {
-        var result = underTest.greet(new String[]{"Antoni", "Anna-Maria", "Chloi"});
+        var result = underTest.buildGreeting(new String[]{"Antoni", "Anna-Maria", "Chloi"});
 
         assertThat(result).isEqualTo("Hello, Antoni, Anna-Maria, and Chloi.");
     }
 
     @Test
     void whenMixedCaseNamesArePassed_greetNormallyAndWithShoutsSeperately() {
-        var result = underTest.greet(new String[]{"Antoni", "Anna-Maria", "CHLOI"});
+        var result = underTest.buildGreeting(new String[]{"Antoni", "Anna-Maria", "CHLOI"});
 
-        assertThat(result).isEqualTo("Hello, Antoni and Anna-Maria. AND HELLO CHLOI.");
+        assertThat(result).isEqualTo("Hello, Antoni and Anna-Maria. AND HELLO CHLOI!");
     }
 }
