e6b5fef7337e2d014124da9b4d8e67e6d00bfe19
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-12-31 17:34:37 +0000

    roman-numerals - add test

--- roman-numerals/src/test/java/com/pittacode/RomanConverterTest.java
+++ roman-numerals/src/test/java/com/pittacode/RomanConverterTest.java
@@ -1,12 +1,25 @@
 package com.pittacode;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RomanConverterTest {
 
+//    @ParameterizedTest
+//    @ValueSource(strings = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"})
+//    void whenPasssingInvalidCharacter_throwException() {
+//
+//    }
 
     @Test
-    void doNothing() {
+    void Iequals1() {
+        var converter = new RomanConverter();
 
+        var result = converter.convert("I");
+
+        assertThat(result).isEqualTo(1);
     }
 }
