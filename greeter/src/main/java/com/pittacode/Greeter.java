96fcbce29e0b223a1d983d85071b120ec2d27a2b
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2020-12-31 13:20:52 +0000

    Greeter - refactor, extract classes

--- greeter/src/main/java/com/pittacode/Greeter.java
+++ greeter/src/main/java/com/pittacode/Greeter.java
@@ -1,8 +1,8 @@
 package com.pittacode;
 
-import java.util.Arrays;
-
-import static java.lang.String.format;
+import static com.pittacode.NameUtils.isEmptyName;
+import static com.pittacode.NameUtils.isShoutedName;
+import static com.pittacode.NameUtils.splitAnyCommaSeparatedEntriesToSingleNames;
 
 public class Greeter {
 
@@ -20,40 +20,14 @@ public class Greeter {
         return new GreeterJoiner(normalGreeter, shoutingGreeter).joinGreetings();
     }
 
-    private String[] splitAnyCommaSeparatedEntriesToSingleNames(String[] names) {
-        return Arrays.stream(names)
-                     .map(this::splitNames)
-                     .flatMap(name -> Arrays.stream(name.clone()).map(String::trim))
-                     .toArray(String[]::new);
-    }
-
-    private String[] splitNames(String name) {
-        return isNameSurrounedWithQuotes(name)
-                ? new String[]{removeQuotes(name)}
-                : name.split(",");
-    }
-
-    private boolean isNameSurrounedWithQuotes(String name) {
-        return name.startsWith("\"") && name.endsWith("\"");
-    }
-
-    private String removeQuotes(String name) {
-        return name.substring(1, name.length() - 1);
-    }
 
     public String buildGreeting(String name) {
-        if (isEmpty(name)) {
+        if (isEmptyName(name)) {
             return new GenericGreeter().buildGreeting();
         }
 
-        return NameUtils.isShoutedName(name)
+        return isShoutedName(name)
                 ? new ShoutingGreeter(name).buildGreeting()
                 : new NormalGreeter(name).buildGreeting();
     }
-
-    private boolean isEmpty(String name) {
-        return name == null || name.isEmpty();
-    }
-
-
 }
--- greeter/src/main/java/com/pittacode/NameUtils.java
+++ greeter/src/main/java/com/pittacode/NameUtils.java
@@ -1,11 +1,45 @@
 package com.pittacode;
 
+import java.util.Arrays;
+
 public final class NameUtils {
+
+    public static boolean isEmptyName(String name) {
+        return name == null || name.isEmpty();
+    }
 
     public static boolean isShoutedName(String name) {
         return name.codePoints()
                    .mapToObj(i -> (char) i)
                    .allMatch(ch -> !Character.isLetter(ch) || Character.isUpperCase(ch));
+    }
+
+    public static String[] splitAnyCommaSeparatedEntriesToSingleNames(String[] names) {
+        return Arrays.stream(names)
+                     .map(NameUtils::splitNames)
+                     .map(NameUtils::trimNames)
+                     .flatMap(arr -> Arrays.stream(arr.clone()))
+                     .toArray(String[]::new);
+    }
+
+    private static String[] trimNames(String[] names) {
+        return Arrays.stream(names)
+                     .map(String::trim)
+                     .toArray(String[]::new);
+    }
+
+    private static String[] splitNames(String name) {
+        return isNameSurrounedWithQuotes(name)
+                ? new String[]{removeQuotes(name)}
+                : name.split(",");
+    }
+
+    private static boolean isNameSurrounedWithQuotes(String name) {
+        return name.startsWith("\"") && name.endsWith("\"");
+    }
+
+    private static String removeQuotes(String name) {
+        return name.substring(1, name.length() - 1);
     }
 
     private NameUtils(){}
