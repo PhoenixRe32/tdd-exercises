7b9652027a401d778983c803e10b15b9694c2df5
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-01-23 17:14:52 +0000

    roman-numerals - add test

--- roman-numerals/src/test/java/com/pittacode/RomanConverterTest.java
+++ roman-numerals/src/test/java/com/pittacode/RomanConverterTest.java
@@ -1,12 +1,16 @@
 package com.pittacode;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class RomanConverterTest {
 
@@ -23,45 +27,25 @@ class RomanConverterTest {
         assertThatThrownBy(() -> converter.convert(input)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void Iequals1() {
-        var result = converter.convert("I");
-        assertThat(result).isEqualTo(1);
+    @ParameterizedTest
+    @MethodSource("romanNumeralsAndTheirValues")
+    void romanNumeralInput_convertsToTheExpectedValue(String romanNumeral, int decimalValue) {
+        var result = converter.convert(romanNumeral);
+
+        assertThat(result).isEqualTo(decimalValue);
     }
 
-    @Test
-    void Vequals5() {
-        var result = converter.convert("V");
-        assertThat(result).isEqualTo(5);
-    }
-
-    @Test
-    void Xequals10() {
-        var result = converter.convert("X");
-        assertThat(result).isEqualTo(10);
-    }
-
-    @Test
-    void Lequals50() {
-        var result = converter.convert("L");
-        assertThat(result).isEqualTo(50);
-    }
-
-    @Test
-    void Cequals100() {
-        var result = converter.convert("C");
-        assertThat(result).isEqualTo(100);
-    }
-
-    @Test
-    void Dequals500() {
-        var result = converter.convert("D");
-        assertThat(result).isEqualTo(500);
-    }
-
-    @Test
-    void Mequals1000() {
-        var result = converter.convert("M");
-        assertThat(result).isEqualTo(1000);
+    private static Stream<Arguments> romanNumeralsAndTheirValues() {
+        return Stream.of(
+                arguments("I", 1),
+                arguments("II", 2),
+                arguments("III", 3),
+                arguments("V", 5),
+                arguments("X", 10),
+                arguments("L", 50),
+                arguments("C", 100),
+                arguments("D", 500),
+                arguments("M", 1000)
+        );
     }
 }
