67444653f68d58f05b243997f8c72cc769fdab13
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-01-25 11:36:56 +0000

    Recently Used List - Initial commit

---
+++ recently-used-list/src/main/java/com/pittacode/recently/RecentlyUsedList.java
@@ -0,0 +1,4 @@
+package com.pittacode.recently;
+
+public class RecentlyUsedList {
+}
---
+++ recently-used-list/src/test/java/com/pittacode/recently/RecentlyUsedListTest.java
@@ -0,0 +1,11 @@
+package com.pittacode.recently;
+
+import org.junit.jupiter.api.Test;
+
+public class RecentlyUsedListTest {
+
+    @Test
+    void doNothing() {
+        var underTest = new RecentlyUsedList();
+    }
+}
