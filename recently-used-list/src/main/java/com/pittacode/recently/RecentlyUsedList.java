eb519fa2a57b2dddf6d32833f15816f4eff60275
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-02-13 14:21:42 +0000

    Recently Used List - Get the elements in LIFO order using 0-based index
    * The most recently added item is first, the least recently added item is last.
    * Items can be looked up by the index, which counts from zero.

--- recently-used-list/src/main/java/com/pittacode/recently/RecentlyUsedList.java
+++ recently-used-list/src/main/java/com/pittacode/recently/RecentlyUsedList.java
@@ -1,4 +1,22 @@
 package com.pittacode.recently;
 
 public class RecentlyUsedList {
+
+    private static final int DEFAULT_SIZE = 5;
+
+    private String[] elements = new String[DEFAULT_SIZE];
+    private int size;
+
+    public int size() {
+        return size;
+    }
+
+    public void add(String element) {
+        elements[size] = element;
+        size++;
+    }
+
+    public String get(int index) {
+        return elements[size-1-index];
+    }
 }
--- recently-used-list/src/test/java/com/pittacode/recently/RecentlyUsedListTest.java
+++ recently-used-list/src/test/java/com/pittacode/recently/RecentlyUsedListTest.java
@@ -1,11 +1,44 @@
 package com.pittacode.recently;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RecentlyUsedListTest {
 
+    private RecentlyUsedList underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new RecentlyUsedList();
+    }
+
     @Test
-    void doNothing() {
+    void shouldHaveSizeOf1_afterAddingOneItem() {
+        underTest.add("element-0");
+
+        assertThat(underTest.size()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldHaveSizeOf2_afterAddingTwoItems() {
         var underTest = new RecentlyUsedList();
+
+        underTest.add("element-0");
+        underTest.add("element-1");
+
+        assertThat(underTest.size()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldStoreElementsInLIFO() {
+        var underTest = new RecentlyUsedList();
+
+        underTest.add("element-0");
+        underTest.add("element-1");
+
+        assertThat(underTest.get(0)).isEqualTo("element-1");
+        assertThat(underTest.get(1)).isEqualTo("element-0");
     }
 }
