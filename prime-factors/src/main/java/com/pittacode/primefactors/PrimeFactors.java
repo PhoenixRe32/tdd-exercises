2d1aa2584108744b31bd67f7a66417fedeb43911
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-02-21 20:48:29 +0000

    PrimeFactors - handle some basic examples
    Experimenting to see if some shape of what it might look might appear.
    Throw exception when can't handle for log purposes in future experimentations.

--- prime-factors/src/main/java/com/pittacode/primefactors/PrimeFactors.java
+++ prime-factors/src/main/java/com/pittacode/primefactors/PrimeFactors.java
@@ -1,5 +1,6 @@
 package com.pittacode.primefactors;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class PrimeFactors {
@@ -13,6 +14,24 @@ public class PrimeFactors {
     public List<Integer> calculate() {
         if (number < 2)
             return List.of();
-        return List.of();
+
+        var result = new LinkedList<Integer>();
+        var intermediateNumber = number;
+        do {
+            if (intermediateNumber % 2 == 0) {
+                intermediateNumber /= 2;
+                result.add(2);
+            } else if (intermediateNumber % 3 == 0) {
+                intermediateNumber /= 3;
+                result.add(3);
+            } else {
+                var message = number + " couldn't be calculated.\n"
+                        + "Factors calculated: " + result + "\n"
+                        + "Remainder: " + intermediateNumber;
+                throw new UnsupportedOperationException(message);
+            }
+        } while (intermediateNumber != 1);
+
+        return result;
     }
 }
--- prime-factors/src/test/java/com/pittacode/primefactors/PrimeFactorsTest.java
+++ prime-factors/src/test/java/com/pittacode/primefactors/PrimeFactorsTest.java
@@ -1,6 +1,5 @@
 package com.pittacode.primefactors;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -13,7 +12,24 @@ public class PrimeFactorsTest {
     @ValueSource(ints = {1, 0, -20})
     void shouldReturnEmptyList_whenLessThan2(int number) {
         var underTest = new PrimeFactors(number);
-
         assertThat(underTest.calculate()).isEmpty();
+    }
+
+    @Test
+    void shouldReturn2_whenNumberIs2() {
+        var underTest = new PrimeFactors(2);
+        assertThat(underTest.calculate()).containsExactly(2);
+    }
+
+    @Test
+    void shouldReturn3_whenNumberIs3() {
+        var underTest = new PrimeFactors(3);
+        assertThat(underTest.calculate()).containsExactly(3);
+    }
+
+    @Test
+    void shouldReturn2_2_whenNumberIs4() {
+        var underTest = new PrimeFactors(4);
+        assertThat(underTest.calculate()).containsExactly(2, 2);
     }
 }
