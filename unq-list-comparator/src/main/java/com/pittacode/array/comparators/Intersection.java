a326c071c434aa3316d85d21ac59f56c270a3644
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-02-27 16:28:10 +0000

    ArrayComparator - Compare arrays of different sizes

--- unq-list-comparator/src/main/java/com/pittacode/array/comparators/Intersection.java
+++ unq-list-comparator/src/main/java/com/pittacode/array/comparators/Intersection.java
@@ -1,11 +1,14 @@
 package com.pittacode.array.comparators;
 
 public class Intersection {
+
     public int calculate(int[] arr1, int[] arr2) {
         var commonElements = 0;
-        for (int i = 0, length = arr1.length; i < length; i++) {
-            if (arr1[i] == arr2[i]) {
-                commonElements++;
+        for (int i = 0, length1 = arr1.length; i < length1; i++) {
+            for (int j = 0, length2 = arr2.length; j < length2; j++) {
+                if (arr1[i] == arr2[j]) {
+                    commonElements++;
+                }
             }
         }
         return commonElements;
--- unq-list-comparator/src/test/java/com/pittacode/array/comparators/IntersectionTest.java
+++ unq-list-comparator/src/test/java/com/pittacode/array/comparators/IntersectionTest.java
@@ -2,6 +2,11 @@ package com.pittacode.array.comparators;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,5 +37,21 @@ class IntersectionTest {
         var intersection = new Intersection();
         var result = intersection.calculate(new int[]{3}, new int[]{3});
         assertThat(result).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @MethodSource("arraysWithOneCommonElement")
+    void shouldReturn1_whenArraysAreOfDifferentSizeAndHaveOneCommonElement(int[] array1, int[] array2) {
+        var intersection = new Intersection();
+        var result = intersection.calculate(array1, array2);
+        assertThat(result).isEqualTo(1);
+    }
+
+    private static Stream<Arguments> arraysWithOneCommonElement() {
+        return Stream.of(
+                Arguments.of(new int[]{10, 3}, new int[]{3}),
+                Arguments.of(new int[]{10, 3}, new int[]{3, 5}),
+                Arguments.of(new int[]{10, 3, 5, 12, 54, 34}, new int[]{0, -10, -34, -23, 3})
+        );
     }
 }
