74e869d43e42b1a3bc3dd2a35b42d4c15a4d9813
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-03-03 19:45:06 +0000

    MagnetParticulars - Implement algorithm based on README

--- magnet-particules/src/main/java/com/pittacode/magnet/particulars/Magents.java
+++ magnet-particules/src/main/java/com/pittacode/magnet/particulars/Magents.java
@@ -1,8 +1,32 @@
 package com.pittacode.magnet.particulars;
 
+import java.util.stream.IntStream;
+
+import static java.lang.StrictMath.pow;
+
 public class Magents {
 
     public double doubles(int maxK, int maxN) {
-        throw new UnsupportedOperationException();
+        return forceExertedByBox(maxK, maxN);
+    }
+
+    private double forceExertedByBox(int maxK, int maxN) {
+        return IntStream.rangeClosed(1, maxK)
+                        .mapToDouble(k -> forceExertedByRow(k, maxN))
+                        .sum();
+    }
+
+    private double forceExertedByRow(int k, int maxN) {
+        return IntStream.rangeClosed(1, maxN)
+                        .mapToDouble(n -> forceExertedByPoint(k, n))
+                        .sum();
+    }
+
+    private double forceExertedByPoint(int k, int n) {
+        return invertOf(k * pow(n + 1, 2 * k));
+    }
+
+    private double invertOf(double denominator) {
+        return 1 / denominator;
     }
 }
