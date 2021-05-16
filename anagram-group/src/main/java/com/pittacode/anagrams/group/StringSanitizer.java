5a4cfbc5d636d717d40450ef663e68f8d34e57cf
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-05-16 16:56:29 +0100

    Sanitize string   * Add regex alternative (curiosity)   * Normalise space; any space should be length of one

--- anagram-group/src/main/java/com/pittacode/anagrams/group/StringSanitizer.java
+++ anagram-group/src/main/java/com/pittacode/anagrams/group/StringSanitizer.java
@@ -1,14 +1,27 @@
 package com.pittacode.anagrams.group;
 
+import java.util.regex.Pattern;
+
 import static com.pittacode.anagrams.group.CharacterHelpers.characterCollector;
 
 public class StringSanitizer {
 
     private static final char SPACE = ' ';
+    private static final Pattern NON_ALPHABETIC = Pattern.compile("[^a-zA-Z ]");
+    private static final Pattern MULTIPLE_SPACE = Pattern.compile("  +");
 
     public String sanitize(String input) {
-        return input.codePoints()
-                    .mapToObj(i -> Character.isAlphabetic(i) ? (char) i : SPACE)
-                    .collect(characterCollector());
+        var sanitizedString = input.codePoints()
+                                   .mapToObj(i -> Character.isAlphabetic(i) ? (char) i : SPACE)
+                                   .collect(characterCollector());
+        return normaliseSpace(sanitizedString);
+    }
+
+    public String sanitizeUsingRegExp(String input) {
+        return normaliseSpace(NON_ALPHABETIC.matcher(input).replaceAll(String.valueOf(SPACE)));
+    }
+
+    private String normaliseSpace(String input) {
+        return MULTIPLE_SPACE.matcher(input).replaceAll(String.valueOf(SPACE));
     }
 }
--- anagram-group/src/test/java/com/pittacode/anagrams/group/StringSanitizerTest.java
+++ anagram-group/src/test/java/com/pittacode/anagrams/group/StringSanitizerTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringSanitizerTest {
 
-    private static final String NON_ALPHABETIC_CHARS = "±!@£$%^&*()_+=-§:\"|';?><,./~`'";
+    private static final String NON_ALPHABETIC_CHARS = "±!@£$%^&*()_+=-§:\"|';?><,./~`' ";
 
     private StringSanitizer underTest;
 
@@ -20,5 +20,23 @@ public class StringSanitizerTest {
     void shouldRemoveNonAlphanumericCharactersFromString() {
         var result  = underTest.sanitize(NON_ALPHABETIC_CHARS);
         assertThat(result.isBlank()).isTrue();
+    }
+
+    @Test
+    void shouldRemoveNonAlphanumericCharactersFromStringUsingRegExp() {
+        var result  = underTest.sanitizeUsingRegExp(NON_ALPHABETIC_CHARS);
+        assertThat(result.isBlank()).isTrue();
+    }
+
+    @Test
+    void shouldReduceMultipleEmptySpacesToSingleSpace() {
+        var result = underTest.sanitize(NON_ALPHABETIC_CHARS);
+        assertThat(result.length()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldReduceInbetweenEmptySpaceToSingleSpace() {
+        var result  = underTest.sanitize("A    A !@££ 2!@£A");
+        assertThat(result).isEqualTo("A A A");
     }
 }
