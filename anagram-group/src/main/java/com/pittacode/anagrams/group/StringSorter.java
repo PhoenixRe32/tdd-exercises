0bcfb695660376291c31f669fff40348cfdc73e5
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-05-16 15:17:31 +0100

    Sort characters of string input   * Return itself when string is singe character

---
+++ anagram-group/src/main/java/com/pittacode/anagrams/group/StringSorter.java
@@ -0,0 +1,8 @@
+package com.pittacode.anagrams.group;
+
+public class StringSorter {
+
+    public String sort(String input) {
+        return input;
+    }
+}
---
+++ anagram-group/src/test/java/com/pittacode/anagrams/group/StringSorterTest.java
@@ -0,0 +1,26 @@
+package com.pittacode.anagrams.group;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER;
+import static org.junit.jupiter.params.ParameterizedTest.DISPLAY_NAME_PLACEHOLDER;
+
+public class StringSorterTest {
+
+    private StringSorter underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new StringSorter();
+    }
+
+    @ParameterizedTest(name = ARGUMENTS_WITH_NAMES_PLACEHOLDER + " -> " + DISPLAY_NAME_PLACEHOLDER)
+    @ValueSource(strings = {"a", "%", "1", "D", " "})
+    void shouldReturnInputWhenItIsSingleCharacter(String input) {
+        var result = underTest.sort(input);
+        assertThat(result).isEqualTo(input);
+    }
+}
