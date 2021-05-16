b9f8e235aa4bbcc8bf60194cb3df73b3b9c5bb94
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-05-16 16:18:02 +0100

    Sort characters of string input   * Return sorted characters of string   * Refactor

--- anagram-group/src/main/java/com/pittacode/anagrams/group/StringCharacterSorter.java
+++ anagram-group/src/main/java/com/pittacode/anagrams/group/StringCharacterSorter.java
@@ -1,7 +1,9 @@
 package com.pittacode.anagrams.group;
 
-import java.util.Objects;
 import java.util.regex.Pattern;
+import java.util.stream.Collector;
+
+import static java.util.Objects.requireNonNull;
 
 public class StringCharacterSorter {
 
@@ -9,16 +11,33 @@ public class StringCharacterSorter {
     private static final String EMPTY = "";
 
     public String sort(String input) {
-        Objects.requireNonNull(input, "Can't sort null values");
+        requireNonNull(input, "Can't sort null values");
 
-        if (input.isBlank()) {
-            return "";
-        }
+        if (input.isBlank()) return "";
 
-        return removeEmptySpace(input);
+        var inputWithoutSpaces = removeEmptySpace(input);
+
+        if (isSingleCharacter(inputWithoutSpaces)) return inputWithoutSpaces;
+
+        return sortCharacters(inputWithoutSpaces);
+    }
+
+    private boolean isSingleCharacter(String inputWithoutSpaces) {
+        return inputWithoutSpaces.length() == 1;
     }
 
     private String removeEmptySpace(String input) {
         return SPACE.matcher(input).replaceAll(EMPTY);
+    }
+
+    private String sortCharacters(String inputWithoutSpaces) {
+        return inputWithoutSpaces.codePoints()
+                                 .mapToObj(i -> (char) i)
+                                 .sorted()
+                                 .collect(Collector.of(
+                                         StringBuilder::new, // supply object that will collect characters
+                                         StringBuilder::append, // accumulate each character to supplier above
+                                         StringBuilder::append, // if running in parallel, this is how you combine the parallel accumulators
+                                         StringBuilder::toString)); // once done, finish it with this action
     }
 }
--- anagram-group/src/test/java/com/pittacode/anagrams/group/StringCharacterSorterTest.java
+++ anagram-group/src/test/java/com/pittacode/anagrams/group/StringCharacterSorterTest.java
@@ -3,12 +3,17 @@ package com.pittacode.anagrams.group;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER;
 import static org.junit.jupiter.params.ParameterizedTest.DISPLAY_NAME_PLACEHOLDER;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class StringCharacterSorterTest {
 
@@ -39,8 +44,24 @@ public class StringCharacterSorterTest {
     }
 
     @Test
-    void shouldReturnStringWithSameNumberOfNonEmptyCharacters() {
+    void shouldReturnStringWithSameNumberOfNonWhitespaceCharacters() {
         var result = underTest.sort("aaa aaa");
         assertThat(result.length()).isEqualTo(6);
+    }
+
+    @ParameterizedTest(name = ARGUMENTS_WITH_NAMES_PLACEHOLDER + " -> " + DISPLAY_NAME_PLACEHOLDER)
+    @MethodSource("stringsAndTheirSortedCharacters")
+    void shouldReturnNonWhitespaceCharactersInAlphabeticalOrder(String input, String expectedOutput) {
+        var result = underTest.sort(input);
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+
+    private static Stream<Arguments> stringsAndTheirSortedCharacters() {
+        return Stream.of(
+                arguments("cab", "abc"),
+                arguments("fedcba321", "123abcdef"),
+                arguments("2wsx", "2swx"),
+                arguments("0123456789abcdefg", "0123456789abcdefg")
+        );
     }
 }
