02d1d87526f70693a3fe0bf711f37160e70833f9
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-05-16 17:22:03 +0100

    Group anagrams (One string)   * Return map

--- anagram-group/src/main/java/com/pittacode/anagrams/group/AnagramGrouper.java
+++ anagram-group/src/main/java/com/pittacode/anagrams/group/AnagramGrouper.java
@@ -1,4 +1,12 @@
 package com.pittacode.anagrams.group;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 public class AnagramGrouper {
+
+    public Map<String, Collection<String>> group(String string) {
+        return Map.of(string, Set.of(string));
+    }
 }
--- anagram-group/src/test/java/com/pittacode/anagrams/group/AnagramGrouperTest.java
+++ anagram-group/src/test/java/com/pittacode/anagrams/group/AnagramGrouperTest.java
@@ -1,11 +1,24 @@
 package com.pittacode.anagrams.group;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnagramGrouperTest {
 
+    private AnagramGrouper underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new AnagramGrouper();
+    }
+
     @Test
-    void nothing() {
-        var underTest = new AnagramGrouper();
+    void shouldReturnMapWithSingleEntryWhenSingleStringIsPassed() {
+        var result = underTest.group("First String");
+        assertThat(result).hasSize(1);
     }
 }
