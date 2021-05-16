0daa2d6e6564007ec1f04125d9da9ed82f3754ad
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-05-16 18:08:28 +0100

    Group anagrams   * Support multiple strings

--- anagram-group/src/main/java/com/pittacode/anagrams/group/AnagramGrouper.java
+++ anagram-group/src/main/java/com/pittacode/anagrams/group/AnagramGrouper.java
@@ -3,6 +3,8 @@ package com.pittacode.anagrams.group;
 import com.pittacode.anagrams.group.key.KeyGenerator;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,9 +16,26 @@ public class AnagramGrouper {
         this.keyGenerator = keyGenerator;
     }
 
+    public Map<String, Collection<String>> group(String anagram) {
+        return group(Set.of(anagram));
+    }
 
-    public Map<String, Collection<String>> group(String string) {
-        var key = keyGenerator.generate(string);
-        return Map.of(key, Set.of(string));
+    public Map<String, Collection<String>> group(Collection<String> anagrams) {
+        Map<String, Collection<String>> groups = new HashMap<>();
+        for (var anagram : anagrams) {
+            var key = keyGenerator.generate(anagram);
+            if (groups.containsKey(key)) {
+                groups.get(key).add(anagram);
+            } else {
+                groups.put(key, initialiseAnagramGroup(anagram));
+            }
+        }
+        return groups;
+    }
+
+    private Set<String> initialiseAnagramGroup(String anagram) {
+        var collectedAnagrams = new HashSet<String>();
+        collectedAnagrams.add(anagram);
+        return collectedAnagrams;
     }
 }
--- anagram-group/src/test/java/com/pittacode/anagrams/group/AnagramGrouperTest.java
+++ anagram-group/src/test/java/com/pittacode/anagrams/group/AnagramGrouperTest.java
@@ -1,10 +1,11 @@
 package com.pittacode.anagrams.group;
 
-import com.pittacode.anagrams.group.key.KeyGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static com.pittacode.anagrams.group.key.KeyGenerator.keyGenerator;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,6 +33,73 @@ public class AnagramGrouperTest {
         void shouldHaveTheSortedStringCharactersAsKey() {
             var result = underTest.group("First String");
             assertThat(result).containsOnlyKeys("FGIINRRSSTT");
+        }
+    }
+
+    @Nested
+    @DisplayName("When multiple strings are passed that are anagrams of each other")
+    class WhenMultipleStringOfSameAnagram {
+
+        @BeforeEach
+        void setUp() {
+            underTest = new AnagramGrouper(keyGenerator());
+        }
+
+        @Test
+        void shouldReturnMapWithSingleEntry() {
+            var result = underTest.group(Set.of("A Snared",
+                                                "Ad Earns",
+                                                "Ad Nears",
+                                                "Read San",
+                                                "Dare San",
+                                                "Dear San",
+                                                "Reads An",
+                                                "Rand Sea",
+                                                "Sand Are",
+                                                "Sand Ear",
+                                                "Sand Era",
+                                                "Rad Sane",
+                                                "andreas"));
+            assertThat(result).hasSize(1);
+            assertThat(result).containsOnlyKeys("AADENRS");
+        }
+    }
+
+    @Nested
+    @DisplayName("When multiple anagrams are passed")
+    class WhenMultipleAnagrams {
+
+        @BeforeEach
+        void setUp() {
+            underTest = new AnagramGrouper(keyGenerator());
+        }
+
+        @Test
+        void shouldReturnMapWithSameNumberOfEntriesAsTheAnagrams() {
+            var anagram1 = "andreas";
+            var anagram2 = "eleni";
+            var anagram3 = "alexis";
+            var result = underTest.group(Set.of(anagram1, anagram2, anagram3));
+            assertThat(result).hasSize(3);
+            assertThat(result).containsOnlyKeys("AADENRS", "EEILN", "AEILSX");
+            assertThat(result.get("AADENRS")).containsExactly(anagram1);
+            assertThat(result.get("EEILN")).containsExactly(anagram2);
+            assertThat(result.get("AEILSX")).containsExactly(anagram3);
+        }
+
+
+        @Test
+        void shouldReturnMapWithSameNumberOfEntriesAsTheAnagramsAndTheCorrectEntriesUnderEachKey() {
+            var anagram1a = "andreas";
+            var anagram1b = "Sand Era";
+            var anagram2a = "eleni";
+            var anagram2b = "Eel In";
+            var anagram2c = "Lee In";
+            var result = underTest.group(Set.of(anagram1a, anagram1b, anagram2a, anagram2b, anagram2c));
+            assertThat(result).hasSize(2);
+            assertThat(result).containsOnlyKeys("AADENRS", "EEILN");
+            assertThat(result.get("AADENRS")).containsExactlyInAnyOrder(anagram1a, anagram1b);
+            assertThat(result.get("EEILN")).containsExactlyInAnyOrder(anagram2a, anagram2b, anagram2c);
         }
     }
 }
