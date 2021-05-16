89d62174e5fea3aebae048c0ea04eb54f722d4bb
Andreas Ανδρέου <andreas.andreou.dev@gmail.com>
2021-05-16 17:35:13 +0100

    Group anagrams (One string)   * Return sorted characters for string as key

--- anagram-group/src/main/java/com/pittacode/anagrams/group/AnagramGrouper.java
+++ anagram-group/src/main/java/com/pittacode/anagrams/group/AnagramGrouper.java
@@ -1,12 +1,24 @@
 package com.pittacode.anagrams.group;
 
+import com.pittacode.anagrams.group.helpers.StringCharacterSorter;
+import com.pittacode.anagrams.group.helpers.StringSanitizer;
+
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 public class AnagramGrouper {
 
+    private final StringSanitizer stringSanitizer;
+    private final StringCharacterSorter stringCharacterSorter;
+
+    public AnagramGrouper(StringSanitizer stringSanitizer, StringCharacterSorter stringCharacterSorter) {
+        this.stringSanitizer = stringSanitizer;
+        this.stringCharacterSorter = stringCharacterSorter;
+    }
+
     public Map<String, Collection<String>> group(String string) {
-        return Map.of(string, Set.of(string));
+        var key = stringCharacterSorter.sort(stringSanitizer.sanitize(string));
+        return Map.of(key, Set.of(string));
     }
 }
--- anagram-group/src/test/java/com/pittacode/anagrams/group/AnagramGrouperTest.java
+++ anagram-group/src/test/java/com/pittacode/anagrams/group/AnagramGrouperTest.java
@@ -1,24 +1,42 @@
 package com.pittacode.anagrams.group;
 
+import com.pittacode.anagrams.group.helpers.StringCharacterSorter;
+import com.pittacode.anagrams.group.helpers.StringSanitizer;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnagramGrouperTest {
 
     private AnagramGrouper underTest;
+    private StringSanitizer stringSanitizer;
+    private StringCharacterSorter stringCharacterSorter;
 
-    @BeforeEach
-    void setUp() {
-        underTest = new AnagramGrouper();
-    }
+    @Nested
+    @DisplayName("When single string is passed")
+    class WhenSingleString {
 
-    @Test
-    void shouldReturnMapWithSingleEntryWhenSingleStringIsPassed() {
-        var result = underTest.group("First String");
-        assertThat(result).hasSize(1);
+        @BeforeEach
+        void setUp() {
+            stringSanitizer = new StringSanitizer();
+            stringCharacterSorter = new StringCharacterSorter();
+            underTest = new AnagramGrouper(stringSanitizer, stringCharacterSorter);
+        }
+
+        @Test
+        void shouldReturnMapWithSingleEntry() {
+            var result = underTest.group("First String");
+            assertThat(result).hasSize(1);
+        }
+
+
+        @Test
+        void shouldHaveTheSortedStringCharactersAsKey() {
+            var result = underTest.group("First String");
+            assertThat(result).containsOnlyKeys("FGIINRRSSTT");
+        }
     }
 }
